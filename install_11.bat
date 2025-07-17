deb53da94966df529c6ef656bca61f74dd95469f
cheesebellies <84882131+cheesebellies@users.noreply.github.com>
2025-07-17 17:32:52 -0400

    Update install_11.bat

--- install_11.bat
+++ install_11.bat
@@ -1,16 +1,16 @@
 @echo off
 setlocal
 
-winget install -e --id Python.Python.3.11
-winget install --id=ChrisBagwell.SoX  -e
+winget install -e --id Python.Python.3.11 --accept-package-agreements --accept-source-agreements
+start /wait winget install --id=ChrisBagwell.SoX  -e --accept-package-agreements --accept-source-agreements
 
 python -m venv .venv
 call .venv\Scripts\activate.bat
 
-python -m pip install --upgrade pip
+python -m pip install --upgrade pip -q --disable-pip-version-check
 
 mkdir books 2>nul
 mkdir voices 2>nul
 
-pip install -r requirements.txt
-
+pip install --no-input -r requirements.txt
+pause
